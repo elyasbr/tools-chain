132e491d3f414dd86abb4295bab302fddf7d1bbc
elyasbr <elyas.gh1985@gmail.com>
2024-11-19 21:11:02 +0330

    version 1.0.11

---
+++ src/crypto.error.ts
@@ -0,0 +1,8 @@
+const section ='Crypto'
+export const  CryptoError   =  {
+  "CRYPTO_NOT_FOUND" :{ section , info : "ASSET_NOT_FOUND" , msg : "", code : 1000 } ,
+
+
+
+}
+
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import  {StructError} from "./struct.error"
 import  {ChainError} from "./chain.error"
 import  {ArchError} from "./arch.error"
 import  {AssetError} from "./asset.error"
+import  {CryptoError} from "./crypto.error"
 
-export  { StructError  , ChainError,ArchError , AssetError}
+
+export  { StructError  , ChainError,ArchError , AssetError , CryptoError}
