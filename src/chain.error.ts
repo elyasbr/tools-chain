70d4c824bacd7061cee42ab4895538efa5a6f196
elyasbr <elyas.gh1985@gmail.com>
2024-11-19 13:43:33 +0330

    version 1.0.7

--- src/chain.error.ts
+++ src/chain.error.ts
@@ -1,7 +1,7 @@
 const section ='Chain'
 export const  ChainError   =  {
   "CHAIN_NOT_FOUND" :{ section , info : "CHAIN_NOT_FOUND" , code : 1000 } ,
-  "CHAIN_HAVE_CHILD" :{ section , info : "CHAIN_HAVE_CHILD" , code : 1001 } ,
+  "CHAIN_HAVE_ARCH" :{ section , info : "CHAIN_HAVE_ARCH" , code : 1001 } ,
 
 
 }
