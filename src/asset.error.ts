f97a225bd2b39a858a30e3509f67718191b3f2ea
elyasbr <elyas.gh1985@gmail.com>
2024-11-19 14:29:10 +0330

    version 1.0.10

---
+++ src/asset.error.ts
@@ -0,0 +1,8 @@
+const section ='Asset'
+export const  AssetError   =  {
+  "ASSET_NOT_FOUND" :{ section , info : "ASSET_NOT_FOUND" , msg : "", code : 1000 } ,
+
+
+
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import  {StructError} from "./struct.error"
 import  {ChainError} from "./chain.error"
 import  {ArchError} from "./arch.error"
+import  {AssetError} from "./asset.error"
 
-export  { StructError  , ChainError,ArchError}
+export  { StructError  , ChainError,ArchError , AssetError}
