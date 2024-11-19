195620d46b99441172ca3b308f0747ff1bbab457
elyasbr <elyas.gh1985@gmail.com>
2024-11-19 13:51:24 +0330

    version 1.0.9

--- src/struct.error.ts
+++ src/struct.error.ts
@@ -1,6 +1,8 @@
 const section ='Struct'
 export const  StructError   =  {
   "STRUCT_NOT_FOUND" :{ section , info : "STRUCT_NOT_FOUND" , msg : "", code : 1000 } ,
+  "STRUCT_HAVE_CHAIN" :{ section , info : "STRUCT_HAVE_CHAIN" , msg : "", code : 1001 } ,
+
 
 
 }
