d0f18216c3105c40e9aef6fc53ae9fc3430d96f6
elyasbr <elyas.gh1985@gmail.com>
2024-11-20 12:32:58 +0330

    version 1.0.12

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import  {ChainError} from "./chain.error"
 import  {ArchError} from "./arch.error"
 import  {AssetError} from "./asset.error"
 import  {CryptoError} from "./crypto.error"
+import  {IpgError} from "./ipg.error"
 
 
-export  { StructError  , ChainError,ArchError , AssetError , CryptoError}
+export  { StructError  , ChainError,ArchError , AssetError , CryptoError , IpgError}
---
+++ src/ipg.error.ts
@@ -0,0 +1,7 @@
+const section ='Ipg'
+export const  IpgError   =  {
+  "IPG_NOT_FOUND" :{ section , info : "IPG_NOT_FOUND" , msg : "" , code : 1000 } ,
+
+
+}
+
