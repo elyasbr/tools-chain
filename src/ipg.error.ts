70f87c79abf0b638d51d3ecdd62263d55fe20e06
elyasbr <elyas.gh1985@gmail.com>
2024-11-23 00:22:35 +0330

    version 1.0.19

--- src/ipg.error.ts
+++ src/ipg.error.ts
@@ -1,6 +1,10 @@
 const section ='Ipg'
 export const  IpgError   =  {
   "IPG_NOT_FOUND" :{ section , info : "IPG_NOT_FOUND" , msg : "" , code : 1000 } ,
+  "SLUG_IPG_IS_REQUIRED" :{ section , info : "SLUG_IPG_IS_REQUIRED" , msg : "" , code : 1001 } ,
+  "LINK_IPG_IS_REQUIRED" :{ section , info : "LINK_IPG_IS_REQUIRED" , msg : "" , code : 1001 } ,
+
+
 
 
 }
