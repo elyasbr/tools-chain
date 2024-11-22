a714b583614397a528e7d13a53f0219f5ccddf6f
elyasbr <elyas.gh1985@gmail.com>
2024-11-22 10:04:44 +0330

    version 1.0.16

--- src/country.error.ts
+++ src/country.error.ts
@@ -1,6 +1,7 @@
 const section ='Country'
 export const  CountryError   =  {
   "COUNTRY_NOT_FOUND" :{ section , info : "COUNTRY_NOT_FOUND" , msg : "", code : 1000 } ,
+  "COUNTRY_BANK_NOT_FOUND" :{ section , info : "COUNTRY_BANK_NOT_FOUND" , msg : "", code : 1001 } ,
 
 
 
