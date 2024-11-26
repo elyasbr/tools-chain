093a8a8b88523761f24ea176a0e58f51e0b91d3c
elyasbr <elyas.gh1985@gmail.com>
2024-11-27 01:06:11 +0330

    version 1.0.26

--- src/struct.error.ts
+++ src/struct.error.ts
@@ -2,6 +2,11 @@ const section ='Struct'
 export const  StructError   =  {
   "STRUCT_NOT_FOUND" :{ section , info : 'STRUCT_NOT_FOUND' , msg : '', code : 1000 } ,
   "STRUCT_HAVE_CHAIN" :{ section , info : 'STRUCT_HAVE_CHAIN' , msg : '', code : 1001 } ,
+  "PATTERN_STRUCT_ID_IS_WRONG" :{ section , info : 'PATTERN_STRUCT_ID_IS_WRONG' , msg : '', code : 1002 } ,
+  "SLUG_FIELD_STRUCT_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_STRUCT_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "REGEX_FIELD_STRUCT_IS_REQUIRED" :{ section , info : 'REGEX_FIELD_STRUCT_IS_REQUIRED' , msg : '' , code : 1004 } ,
+  "SYMBOL_FIELD_STRUCT_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_STRUCT_IS_REQUIRED' , msg : '' , code : 1005 } ,
+  "STATUS_FIELD_STRUCT_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_STRUCT_IS_REQUIRED' , msg : '' , code : 1005 } ,
 
 
 
