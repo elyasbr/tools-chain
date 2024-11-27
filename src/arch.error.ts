decc644ee2726d62528d75ffb15f2e2536b10a5c
elyasbr <elyas.gh1985@gmail.com>
2024-11-27 10:13:40 +0330

    version 1.0.28

--- src/arch.error.ts
+++ src/arch.error.ts
@@ -1,6 +1,10 @@
 const section ='Arch'
 export const  ArchError   =  {
-  "Arch_NOT_FOUND" :{ section , info : 'Arch_NOT_FOUND' , msg : '' , code : 1000 } ,
+  "ARCH_NOT_FOUND" :{ section , info : 'ARCH_NOT_FOUND' , msg : '' , code : 1000 } ,
+  "SLUG_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "RATE_WITHDRAW_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'RATE_WITHDRAW_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "SYMBOL_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "STATUS_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1004 } ,
 
 
 }
--- src/chain.error.ts
+++ src/chain.error.ts
@@ -6,6 +6,8 @@ export const  ChainError   =  {
   "REGEX_FIELD_CHAIN_IS_REQUIRED" :{ section , info : 'REGEX_FIELD_CHAIN_IS_REQUIRED' , msg : '' , code : 1004 } ,
   "SYMBOL_FIELD_CHAIN_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_CHAIN_IS_REQUIRED' , msg : '' , code : 1005 } ,
   "STATUS_FIELD_CHAIN_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_CHAIN_IS_REQUIRED' , msg : '' , code : 1006 } ,
+  "PATTERN_ChAIN_ID_IS_WRONG" :{ section , info : 'PATTERN_ChAIN_ID_IS_WRONG' , msg : '', code : 1007 } ,
+
 
 }
 
