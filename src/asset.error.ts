3d5ed560e553372e1d2f2e0aedf16946fdfa8788
elyasbr <elyas.gh1985@gmail.com>
2024-11-27 14:32:07 +0330

    version 1.0.30

--- src/asset.error.ts
+++ src/asset.error.ts
@@ -1,7 +1,18 @@
 const section ='Asset'
 export const  AssetError   =  {
   "ASSET_NOT_FOUND" :{ section , info : 'ASSET_NOT_FOUND' , msg : '', code : 1000 } ,
-
+  "SLUG_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SYMBOL_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "TYPE_ASSET_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'TYPE_ASSET_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "IS_WITHDRAW_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'IS_WITHDRAW_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1004 } ,
+  "IS_DEPOSIT_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'IS_DEPOSIT_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1005 } ,
+  "START_WITHDRAW_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'IS_DEPOSIT_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1006 } ,
+  "START_DEPOSIT_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'START_DEPOSIT_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1007 } ,
+  "START_TRADE_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'START_TRADE_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1008 } ,
+  "DECIMAL_SHOW_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'DECIMAL_SHOW_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1009 } ,
+  "DECIMAL_CALC_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'DECIMAL_CALC_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1010 } ,
+  "LOGO_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'LOGO_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1011 } ,
+  "STATUS_FIELD_ASSET_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ASSET_IS_REQUIRED' , msg : '' , code : 1012 } ,
 
 
 }
