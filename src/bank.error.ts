2560a19c1ea061cce4d46d387390e39429919c46
elyasbr <elyas.gh1985@gmail.com>
2024-11-28 19:15:29 +0330

    version 1.0.33

--- src/bank.error.ts
+++ src/bank.error.ts
@@ -1,6 +1,8 @@
 const section ='Bank'
 export const  BankError   =  {
-  "BANK_NOT_FOUND" :{ section , info : 'BANK_NOT_FOUND' ,msg : '', code : 1000 }
+  "BANK_NOT_FOUND" :{ section , info : 'BANK_NOT_FOUND' ,msg : '', code : 1000 } ,
+  "SLUG_FIELD_BANK_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "STATUS_FIELD_BANK_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1002 } ,
 
 }
 
