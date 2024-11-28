fca4bace65b6ff38dc181838a1ff1c5748593d14
elyasbr <elyas.gh1985@gmail.com>
2024-11-28 17:02:19 +0330

    version 1.0.32

--- src/country.error.ts
+++ src/country.error.ts
@@ -1,9 +1,13 @@
 const section ='Country'
 export const  CountryError   =  {
   "COUNTRY_NOT_FOUND" :{ section , info : 'COUNTRY_NOT_FOUND' , msg : '', code : 1000 } ,
-  "COUNTRY_BANK_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_NOT_FOUND' , msg : '', code : 1001 } ,
-  "COUNTRY_BANK_CRYPTO_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_NOT_FOUND' , msg : '', code : 1002 } ,
-  "COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND' , msg : '', code : 1003 } ,
+  "SLUG_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "ISO2_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO2_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "ISO3_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO3_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "STATUS_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1004 } ,
+  "COUNTRY_BANK_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_NOT_FOUND' , msg : '', code : 1005 } ,
+  "COUNTRY_BANK_CRYPTO_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_NOT_FOUND' , msg : '', code : 1006 } ,
+  "COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND' , msg : '', code : 1007 } ,
 
 
 
