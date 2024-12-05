080f08a5bfbdc999bc3c7f8eff11f26c70b7a89e
elyasbr <elyas.gh1985@gmail.com>
2024-12-05 19:30:52 +0330

    version 1.0.48

--- src/arch.error.ts
+++ src/arch.error.ts
@@ -6,7 +6,8 @@ export const  ArchError   =  {
   "SYMBOL_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "STATUS_FIELD_ARCH_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ARCH_IS_REQUIRED' , msg : '' , code : 1004 } ,
   "PATTERN_ARCH_ID_IS_WRONG" :{ section , info : 'PATTERN_ARCH_ID_IS_WRONG' , msg : '', code : 1005 } ,
-
+  "SLUG_ARCH_IS_DUPLICATED" :{ section , info : 'SLUG_ARCH_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "SYMBOL_ARCH_IS_DUPLICATED" :{ section , info : 'SYMBOL_ARCH_IS_DUPLICATED' , msg : '' , code : 1009 }
 
 }
 
