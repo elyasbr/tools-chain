224aa999866a6a55aa06e85f2e2f001d2abf9575
elyasbr <elyas.gh1985@gmail.com>
2024-12-05 17:19:17 +0330

    version 1.0.46

--- src/ipg.error.ts
+++ src/ipg.error.ts
@@ -9,6 +9,11 @@ export const  IpgError   =  {
   "LINK_FRONT_IPG_IS_REQUIRED" :{ section , info : 'LINK_FRONT_IPG_IS_REQUIRED' , msg : '' , code : 1006 } ,
   "CALL_BACK_FRONT_IPG_IS_REQUIRED" :{ section , info : 'CALL_BACK_FRONT_IPG_IS_REQUIRED' , msg : '' , code : 1007 } ,
   "PATTERN_IPG_ID_IS_WRONG" :{ section , info : 'PATTERN_IPG_ID_IS_WRONG' , msg : '', code : 1008 } ,
+  "SLUG_IPG_IS_DUPLICATED" :{ section , info : 'SLUG_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "LINK_IPG_IS_DUPLICATED" :{ section , info : 'LINK_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "CALL_BACK_IPG_IS_DUPLICATED" :{ section , info : 'CALL_BACK_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "LINK_FRONT_IPG_IS_DUPLICATED" :{ section , info : 'LINK_FRONT_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "CALL_BACK_FRONT_IPG_IS_DUPLICATED" :{ section , info : 'CALL_BACK_FRONT_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
 
 
 
