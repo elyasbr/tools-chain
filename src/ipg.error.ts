37cb03cbbd7e51e413bb55636e3848dc8123d302
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-06 11:40:16 +0330

    version 1.0.53

--- src/ipg.error.ts
+++ src/ipg.error.ts
@@ -3,7 +3,7 @@ const g :number=0
 export const  IpgError   =  {
   "IPG_NOT_FOUND" :{ section , info : 'IPG_NOT_FOUND' , msg : '' , code : 1000 } ,
   "SLUG_IPG_IS_REQUIRED" :{ section , info : 'SLUG_IPG_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "LINK_IPG_IS_REQUIRED" :{ section , info : 'LINK_IPG_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "URL_LINK_IPG_IS_REQUIRED" :{ section , info : 'URL_LINK_IPG_IS_REQUIRED' , msg : '' , code : 1002 } ,
   "CALL_BACK_IPG_IS_REQUIRED" :{ section , info : 'CALL_BACK_IPG_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "TYPE_IPG_IS_REQUIRED" :{ section , info : 'TYPE_IPG_IS_REQUIRED' , msg : '' , code : 1004 } ,
   "STATUS_IPG_IS_REQUIRED" :{ section , info : 'STATUS_IPG_IS_REQUIRED' , msg : '' , code : 1005 } ,
@@ -15,6 +15,7 @@ export const  IpgError   =  {
   "CALL_BACK_IPG_IS_DUPLICATED" :{ section , info : 'CALL_BACK_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "LINK_FRONT_IPG_IS_DUPLICATED" :{ section , info : 'LINK_FRONT_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "CALL_BACK_FRONT_IPG_IS_DUPLICATED" :{ section , info : 'CALL_BACK_FRONT_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "BODY_LINK_IPG_IS_REQUIRED" :{ section , info : 'BODY_LINK_IPG_IS_REQUIRED' , msg : '' , code : 1003 } ,
 
 
 
