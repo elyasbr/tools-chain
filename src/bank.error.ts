c6a050d9a3e9b81f50c6a6290e899c9feb2e67e1
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-07 13:55:37 +0330

    version 1.0.62

--- src/bank.error.ts
+++ src/bank.error.ts
@@ -5,6 +5,8 @@ export const  BankError   =  {
   "STATUS_FIELD_BANK_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1002 } ,
   "PATTERN_BANK_ID_IS_WRONG" :{ section , info : 'PATTERN_BANK_ID_IS_WRONG' , msg : '', code : 1007 } ,
   "SLUG_BANK_IS_DUPLICATED" :{ section , info : 'SLUG_BANK_IS_DUPLICATED' , msg : '' , code : 1008 } ,
+  "SYMBOL_FIELD_BANK_IS_REQUIRED" :{ section , info : 'SYMBOL_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SYMBOL_BANK_IS_DUPLICATED" :{ section , info : 'SYMBOL_BANK_IS_DUPLICATED' , msg : '' , code : 1008 } ,
 
 }
 
