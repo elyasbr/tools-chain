719733af21d4e20aae011cc75e45469642549ec6
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-08 20:14:53 +0330

    version 1.0.73

--- src/speed-withdraw.error.ts
+++ src/speed-withdraw.error.ts
@@ -1,7 +1,9 @@
 const section ='SpeedWithdraw'
 export const  SpeedWithdrawError   =  {
   "SPEED_WITHDRAW_NOT_FOUND" :{ section , info : 'SPEED_WITHDRAW_NOT_FOUND' , msg : '', code : 1000 } ,
-
+  "SLUG_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SLUG_SPEED_WITHDRAW_IS_EMPTY" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "STATUS_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1001 }
 
 
 
