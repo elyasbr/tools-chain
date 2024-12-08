1e91c46f7ab870769002803a70dcfcd748172a27
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-08 20:52:09 +0330

    version 1.0.75

--- src/speed-withdraw.error.ts
+++ src/speed-withdraw.error.ts
@@ -2,9 +2,10 @@ const section ='SpeedWithdraw'
 export const  SpeedWithdrawError   =  {
   "SPEED_WITHDRAW_NOT_FOUND" :{ section , info : 'SPEED_WITHDRAW_NOT_FOUND' , msg : '', code : 1000 } ,
   "SLUG_FIELD_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "SLUG_FIELD_SPEED_WITHDRAW_IS_EMPTY" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "STATUS_FIELD_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "RATE_WITHDRAW_FIELD_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'RATE_WITHDRAW_FIELD_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SLUG_FIELD_SPEED_WITHDRAW_IS_EMPTY" :{ section , info : 'SLUG_SPEED_WITHDRAW_IS_EMPTY' , msg : '' , code : 1002 } ,
+  "STATUS_FIELD_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "RATE_WITHDRAW_FIELD_SPEED_WITHDRAW_IS_REQUIRED" :{ section , info : 'RATE_WITHDRAW_FIELD_SPEED_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1004 } ,
+  "SLUG_FIELD_SPEED_WITHDRAW_IS_DUPLICATED" :{ section , info : 'SLUG_FIELD_SPEED_WITHDRAW_IS_DUPLICATED' , msg : '' , code : 1005 } ,
 
 
 
