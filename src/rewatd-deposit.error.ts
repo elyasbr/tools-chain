815fcfeb711517e3a84427cccb432b53c578e6d4
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-08 11:09:46 +0330

    version 1.0.69

--- src/rewatd-deposit.error.ts
+++ src/rewatd-deposit.error.ts
@@ -1,8 +1,8 @@
 const section ='Country'
 export const  RewardDepositError   =  {
   "REWARD_DEPOSIT_NOT_FOUND" :{ section , info : 'REWARD_DEPOSIT_NOT_FOUND' , msg : '', code : 1000 } ,
-  "PATTERN_CRYPTO-ID_IS_WRONG" :{ section , info : 'PATTERN_CRYPTO-ID_IS_WRONG' , msg : '', code : 1008 } ,
-  "PATTERN_ASSET-ID_IS_WRONG" :{ section , info : 'PATTERN_CRYPTO-ID_IS_WRONG' , msg : '', code : 1008 } ,
+  "PATTERN_CRYPTO_ID_IS_WRONG" :{ section , info : 'PATTERN_CRYPTO_ID_IS_WRONG' , msg : '', code : 1008 } ,
+  "PATTERN_ASSET_ID_IS_WRONG" :{ section , info : 'PATTERN_ASSET_ID_IS_WRONG' , msg : '', code : 1008 } ,
   "STATUS_FIELD_REWARD_DEPOSIT_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_REWARD_DEPOSIT_IS_REQUIRED' , msg : '' , code : 1001 }
 
 
