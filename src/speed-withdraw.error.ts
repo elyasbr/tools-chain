46610d3a8047aec34b520f8c04989dd1e540c3ce
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-08 20:06:29 +0330

    version 1.0.71

---
+++ src/speed-withdraw.error.ts
@@ -0,0 +1,9 @@
+const section ='SpeedWithdraw'
+export const  SpeedWithdraw   =  {
+  "SPEED_WITHDRAW_NOT_FOUND" :{ section , info : 'SPEED_WITHDRAW_NOT_FOUND' , msg : '', code : 1000 } ,
+
+
+
+
+}
+
