53e8fd53132b52548be48be8ce06ff01d644b3bf
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-09 23:03:38 +0330

    version 1.0.81

--- src/fee-withdraw.error.ts
+++ src/fee-withdraw.error.ts
@@ -2,7 +2,8 @@ const section ='SpeedWithdraw'
 export const  FeeWithdrawError   =  {
   "FEE_WITHDRAW_NOT_FOUND" :{ section , info : 'FEE_WITHDRAW_NOT_FOUND' , msg : '', code : 1000 } ,
   "PATTERN_WITHDRAW_CRYPTO_IS_WRONG" :{ section , info : 'PATTERN_WITHDRAW_CRYPTO_IS_WRONG' , msg : '', code : 1008 } ,
-  "FEE_WITHDRAW_FIELD_FEE_WITHDRAW_IS_REQUIRED" :{ section , info : 'FEE_WITHDRAW_FIELD_FEE_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1003 }
+  "FEE_WITHDRAW_FIELD_FEE_WITHDRAW_IS_REQUIRED" :{ section , info : 'FEE_WITHDRAW_FIELD_FEE_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "STATUS_FIELD_FEE_WITHDRAW_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_FEE_WITHDRAW_IS_REQUIRED' , msg : '' , code : 1003 }
 
 
 
