d348a14ba92b828bb92f78405b35d1b8e0b5f7e0
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-09 11:06:23 +0330

    version 1.0.77

--- src/withdraw-crypto.error.ts
+++ src/withdraw-crypto.error.ts
@@ -3,7 +3,9 @@ export const  WithdrawCryptoError   =  {
   "WITHDRAW_CRYPTO_NOT_FOUND" :{ section , info : 'SPEED_WITHDRAW_NOT_FOUND' , msg : '', code : 1000 } ,
   "PATTERN_SPEED_WITHDRAW_ID_IS_WRONG" :{ section , info : 'PATTERN_SPEED_WITHDRAW_ID_IS_WRONG' , msg : '', code : 1008 } ,
   "PATTERN_CRYPTO_ID_IS_WRONG" :{ section , info : 'PATTERN_CRYPTO_ID_IS_WRONG' , msg : '' , code : 1002 } ,
-  "FEE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED" :{ section , info : 'FEE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED' , msg : '' , code : 1003 }
+  "FEE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED" :{ section , info : 'FEE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "RATE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED" :{ section , info : 'RATE_WITHDRAW_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "STATUS_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_WITHDRAW_CRYPTO_IS_REQUIRED' , msg : '' , code : 1003 }
 
 
 }
