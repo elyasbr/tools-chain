b9bfa2ac411d0be9b0c3a1481b194e308193ad58
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-12 22:40:55 +0330

    version 1.0.84

--- src/email.error.ts
+++ src/email.error.ts
@@ -1,5 +1,6 @@
 const section ='Email'
 export const  EmailError   =  {
+  "TOKEN_NOT_FOUND" :{ section , info : 'TOKEN_NOT_FOUND' , msg : '' , code : 1001 } ,
   "EMAIL_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'EMAIL_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
 
 }
