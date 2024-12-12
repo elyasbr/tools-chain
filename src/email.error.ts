ad1890378db51f88cc599a439f8a7d3ee399b738
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-13 01:58:03 +0330

    version 1.0.88

--- src/email.error.ts
+++ src/email.error.ts
@@ -4,7 +4,9 @@ export const  EmailError   =  {
   "EMAIL_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'EMAIL_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "EMAIL_ALREADY_VERIFYED" :{ section , info : 'EMAIL_ALREADY_VERIFYED' , msg : '' , code : 1003 } ,
   "EMAIL_NOT_FOUND" :{ section , info : 'EMAIL_NOT_FOUND' , msg : '' , code : 1001 } ,
-  "EMAIL_ALREADY_REGISTERED" :{ section , info : 'EMAIL_ALREADY_REGISTERED' , msg : '' , code : 1003 }
+  "EMAIL_ALREADY_REGISTERED" :{ section , info : 'EMAIL_ALREADY_REGISTERED' , msg : '' , code : 1003 } ,
+  "TOKEN_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'TOKEN_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "TOKEN_FIELD_EMAIL_IS_EMPTY" :{ section , info : 'TOKEN_FIELD_EMAIL_IS_EMPTY' , msg : '' , code : 1002 }
 
 }
 
