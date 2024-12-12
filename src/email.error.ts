2422875f328390a231b21d49442b17e058197a83
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-13 01:40:05 +0330

    version 1.0.87

--- src/email.error.ts
+++ src/email.error.ts
@@ -4,6 +4,7 @@ export const  EmailError   =  {
   "EMAIL_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'EMAIL_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "EMAIL_ALREADY_VERIFYED" :{ section , info : 'EMAIL_ALREADY_VERIFYED' , msg : '' , code : 1003 } ,
   "EMAIL_NOT_FOUND" :{ section , info : 'EMAIL_NOT_FOUND' , msg : '' , code : 1001 } ,
+  "EMAIL_ALREADY_REGISTERED" :{ section , info : 'EMAIL_ALREADY_REGISTERED' , msg : '' , code : 1003 }
 
 }
 
