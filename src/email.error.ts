effa9923b077880e662b6f973afccadddfaa20a7
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-14 00:05:25 +0330

    version 1.0.93

--- src/email.error.ts
+++ src/email.error.ts
@@ -10,7 +10,9 @@ export const  EmailError   =  {
   "OTP_NOT_FOUND" :{ section , info : 'OTP_NOT_FOUND' , msg : '' , code : 1001 } ,
   "REFRESH_TOKEN_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'REFRESH_TOKEN_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "REFRESH_TOKEN_FIELD_EMAIL_IS_EMPTY" :{ section , info : 'REFRESH_TOKEN_FIELD_EMAIL_IS_EMPTY' , msg : '' , code : 1002 } ,
-  "EMAIL_FROM_EMAIL_IS_DUPLICATED" :{ section , info : 'EMAIL_FROM_EMAIL_IS_DUPLICATED' , msg : '' , code : 1009 }
+  "EMAIL_FROM_EMAIL_IS_DUPLICATED" :{ section , info : 'EMAIL_FROM_EMAIL_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "OTP_CODE_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'OTP_CODE_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "OTP_TOKEN_FIELD_EMAIL_IS_REQUIRED" :{ section , info : 'OTP_TOKEN_FIELD_EMAIL_IS_REQUIRED' , msg : '' , code : 1003 }
 
 }
 
