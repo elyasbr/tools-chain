4ebb7ef55394cdc49c8666db0e0aff7b1fc2081c
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-15 01:15:13 +0330

    version 1.0.96

--- src/backend.error.ts
+++ src/backend.error.ts
@@ -3,7 +3,8 @@ export const  BackendError   =  {
   "BACKEND_NOT_FOUND" :{ section , info : 'BACKEND_NOT_FOUND' , msg : '' , code : 1001 } ,
   "SLUG_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "ROUTE_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'ROUTE_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "SLUG_BACKEND_IS_EMPTY" :{ section , info : 'SLUG_BACKEND_IS_EMPTY' , msg : '' , code : 1001 }
+  "SLUG_BACKEND_IS_EMPTY" :{ section , info : 'SLUG_BACKEND_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "ROUTE_BACKEND_IS_EMPTY" :{ section , info : 'ROUTE_BACKEND_IS_EMPTY' , msg : '' , code : 1001 }
 
 }
 
