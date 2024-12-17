d4e5cf28d9db219a08a0498e486da50431173304
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-17 19:53:01 +0330

    version 1.1.5

--- src/menu.error.ts
+++ src/menu.error.ts
@@ -2,7 +2,9 @@ const section ='MENU'
 export const  MenuError   =  {
   "MENU_NOT_FOUND" :{ section , info : 'MENU_NOT_FOUND' , msg : '', code : 1000 } ,
   "PATTERN_MENU_ID_IS_WRONG" :{ section , info : 'PATTERN_MENU_ID_IS_WRONG' , msg : '', code : 1008 } ,
-  "STATUS_FIELD_MENU_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_MENU_IS_REQUIRED' , msg : '' , code : 1001 }
+  "STATUS_FIELD_MENU_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_MENU_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SLUG_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "SLUG_FIELD_MENU_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_MENU_IS_REQUIRED' , msg : '' , code : 1001 }
 
 }
 
