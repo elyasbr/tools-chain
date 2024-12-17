89c74d5c316b350b59fdfb1754ca6f5505108062
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-17 18:00:08 +0330

    version 1.1.3

--- src/backend.error.ts
+++ src/backend.error.ts
@@ -4,7 +4,10 @@ export const  BackendError   =  {
   "SLUG_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "ROUTE_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'ROUTE_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "SLUG_BACKEND_IS_EMPTY" :{ section , info : 'SLUG_BACKEND_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "ROUTE_BACKEND_IS_EMPTY" :{ section , info : 'ROUTE_BACKEND_IS_EMPTY' , msg : '' , code : 1001 }
+  "ROUTE_BACKEND_IS_EMPTY" :{ section , info : 'ROUTE_BACKEND_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "METHOD_FIELD_IS_EMPTY" :{ section , info : 'METHOD_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "METHOD_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'METHOD_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "STATUS_FIELD_BACKEND_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BACKEND_IS_REQUIRED' , msg : '' , code : 1001 }
 
 }
 
--- src/front.error.ts
+++ src/front.error.ts
@@ -4,11 +4,12 @@ export const  FrontError   =  {
   "PATTERN_FRONT_ID_IS_WRONG" :{ section , info : 'PATTERN_FRONT_ID_IS_WRONG' , msg : '', code : 1008 } ,
   "SLUG_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
   "SLUG_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "METHOD_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'METHOD_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "STATUS_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "STATUS_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "ROUTE_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
   "ROUTE_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "METHOD_FIELD_IS_EMPTY" :{ section , info : 'METHOD_FIELD_IS_EMPTY' , msg : '' , code : 1001 }
+  "METHOD_FIELD_IS_EMPTY" :{ section , info : 'METHOD_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "METHOD_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'METHOD_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 }
+
 
 }
 
