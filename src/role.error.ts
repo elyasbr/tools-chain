bc867f882a6627041385952cf659fbfb3840d542
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-18 02:28:30 +0330

    version 1.1.9

--- src/role.error.ts
+++ src/role.error.ts
@@ -5,7 +5,8 @@ export const  RoleError   =  {
   "SLUG_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
   "SLUG_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "IS_DEFAULT_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'IS_DEFAULT_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "STATUS_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 }
+  "STATUS_FIELD_ROLE_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_ROLE_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "IS_DEFAULT_ROLE_ALREADY_EXISTS" :{ section , info : 'IS_DEFAULT_ROLE_ALREADY_EXISTS' , msg : '' , code : 1001 }
 
 }
 
