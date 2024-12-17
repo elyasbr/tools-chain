2882b323f6a5afc5dfccf65acf17f0a5600b1bcb
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-17 21:20:13 +0330

    version 1.1.7

--- src/auth.error.ts
+++ src/auth.error.ts
@@ -2,7 +2,8 @@ const section ='Auth'
 export const  AuthError   =  {
   "USER_NOT_FOUND" :{ section , info : 'USER_NOT_FOUND' ,msg : '', code : 1000 } ,
   "EMAIL_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1002 } ,
-  "PASSWORD_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1002 }
+  "PASSWORD_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BANK_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "PATTERN_USER_ID_IS_WRONG" :{ section , info : 'PATTERN_USER_ID_IS_WRONG' , msg : '', code : 1005 }
 
 
 
