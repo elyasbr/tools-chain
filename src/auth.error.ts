6670bf1a28cf8a32f99f38fd8d92b4e95d2cdf86
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-21 00:50:05 +0330

    version 1.1.16

--- src/auth.error.ts
+++ src/auth.error.ts
@@ -6,7 +6,8 @@ export const  AuthError   =  {
   "PATTERN_USER_ID_IS_WRONG" :{ section , info : 'PATTERN_USER_ID_IS_WRONG' , msg : '', code : 1005 } ,
   "ROLE_USER_NOT_FOUND" :{ section , info : 'ROLE_USER_NOT_FOUND' ,msg : '', code : 1000 } ,
   "WALLET_ADDRESS_IS_DUPLICATED" :{ section , info : 'WALLET_ADDRESS_IS_DUPLICATED' , msg : '' , code : 1008 } ,
-  "PAYLOAD_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'PAYLOAD_FIELD_AUTH_IS_REQUIRED' , msg : '' , code : 1002 }
+  "PAYLOAD_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'PAYLOAD_FIELD_AUTH_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "PIN_CODE_FIELD_AUTH_IS_REQUIRED" :{ section , info : 'PIN_CODE_FIELD_AUTH_IS_REQUIRED' , msg : '' , code : 1002 }
 
 
 
