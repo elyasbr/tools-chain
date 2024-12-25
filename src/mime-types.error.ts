cb389b659b9dfce4d978dd1f982d565f81762843
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-25 23:36:03 +0330

    version 1.1.23

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -6,6 +6,7 @@ export const  MimeTypeError   =  {
   "PATTERN_DESTINATION_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_DESTINATION_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 } ,
   "MINIMUM_LENGTH_PREFIX_FIELD_MIME_TYPES_IS_TWO" :{ section , info : 'MINIMUM_LENGTH_PREFIX_FIELD_MIME_TYPES_IS_TWO' , msg : '' , code : 1001 } ,
   "MAX_SIZE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MAX_SIZE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "STATUS_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 }
 
 
 
