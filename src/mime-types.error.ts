d133e3cc85bc6f838da735851bfd8c6ea2765fce
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-25 23:28:02 +0330

    version 1.1.22

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -3,7 +3,9 @@ export const  MimeTypeError   =  {
   "MIME_TYPE_NOT_FOUND" :{ section , info : 'MIME_TYPE_NOT_FOUND' ,msg : '', code : 1000 } ,
   "MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "PATTERN_SOURCE_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_SOURCE_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 } ,
-  "PATTERN_DESTINATION_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_DESTINATION_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 }
+  "PATTERN_DESTINATION_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_DESTINATION_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 } ,
+  "MINIMUM_LENGTH_PREFIX_FIELD_MIME_TYPES_IS_TWO" :{ section , info : 'MINIMUM_LENGTH_PREFIX_FIELD_MIME_TYPES_IS_TWO' , msg : '' , code : 1001 } ,
+  "MAX_SIZE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MAX_SIZE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
 
 
 
