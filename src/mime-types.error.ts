e0dd171ef5e4965d60bce69743ef532c3c067b34
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-25 23:20:59 +0330

    version 1.1.21

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -1,6 +1,7 @@
 const section ='MimeType'
 export const  MimeTypeError   =  {
   "MIME_TYPE_NOT_FOUND" :{ section , info : 'MIME_TYPE_NOT_FOUND' ,msg : '', code : 1000 } ,
+  "MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "PATTERN_SOURCE_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_SOURCE_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 } ,
   "PATTERN_DESTINATION_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_DESTINATION_BUCKET_ID_IS_WRONG' , msg : '', code : 1013 }
 
