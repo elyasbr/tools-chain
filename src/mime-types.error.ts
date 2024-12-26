85e0a349fa191ddfc983e47fef064702348b8eb7
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-26 12:04:37 +0330

    version 1.1.28

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -3,7 +3,8 @@ export const  MimeTypeError   =  {
   "MIME_TYPE_NOT_FOUND" :{ section , info : 'MIME_TYPE_NOT_FOUND' ,msg : '', code : 1000 } ,
   "MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "STATUS_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "MIME_TYPE_IS_DUPLICATED" :{ section , info : 'MIME_TYPE_IS_DUPLICATED' , msg : '' , code : 1009 }
+  "MIME_TYPE_IS_DUPLICATED" :{ section , info : 'MIME_TYPE_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "SLUG_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 }
 
 
 
