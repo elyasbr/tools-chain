2127f522cd14a7df56dc1fae192670aae24a5f09
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-26 12:06:47 +0330

    version 1.1.29

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -4,7 +4,8 @@ export const  MimeTypeError   =  {
   "MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'MIME_TYPE_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "STATUS_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "MIME_TYPE_IS_DUPLICATED" :{ section , info : 'MIME_TYPE_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "SLUG_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 }
+  "SLUG_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SLUG_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 }
 
 
 
