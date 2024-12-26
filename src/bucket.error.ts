de8a11223d864b2b1b9bfc347a78dd63b935f4ee
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-26 15:38:27 +0330

    version 1.1.32

--- src/bucket.error.ts
+++ src/bucket.error.ts
@@ -2,7 +2,8 @@ const section ='Bucket'
 export const  BucketError   =  {
   "BUCKET_NOT_FOUND" :{ section , info : 'USER_NOT_FOUND' ,msg : '', code : 1000 } ,
   "SLUG_FIELD_BUCKET_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_BUCKET_IS_REQUIRED' , msg : '' , code : 1002 } ,
-  "SLUG_BUCKET_IS_DUPLICATED" :{ section , info : 'SLUG_BUCKET_IS_DUPLICATED' , msg : '' , code : 1008 }
+  "SLUG_BUCKET_IS_DUPLICATED" :{ section , info : 'SLUG_BUCKET_IS_DUPLICATED' , msg : '' , code : 1008 } ,
+  "MINIMUM_CHARACHTERS_PREFIX_FIELD_BUCKET_IS_3" :{ section , info : 'MINIMUM_CHARACHTERS_PREFIX_FIELD_BUCKET_IS_3' , msg : '' , code : 1001 }
 
 
 
