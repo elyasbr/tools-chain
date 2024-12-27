4139d82ab5490bff873f766773760d795b77abb3
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-27 17:49:48 +0330

    version 1.1.44

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -6,7 +6,10 @@ export const  MimeTypeError   =  {
   "SLUG_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "SLUG_FIELD_MIME_TYPES_IS_EMPTY" :{ section , info : 'SLUG_FIELD_MIME_TYPES_IS_EMPTY' , msg : '' , code : 1001 } ,
   "MINIMUM_CHARACHTERS_PREFIX_FIELD_MIME_TYPES_IS_3" :{ section , info : 'MINIMUM_CHARACHTERS_PREFIX_FIELD_MIME_TYPES_IS_3' , msg : '' , code : 1001 } ,
-  "EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 }
+  "SLUG_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'SLUG_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 }
+
 
 
 
