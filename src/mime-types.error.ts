5d3781d7e3de0ffaedeb4c0246fb1f2bb1b47d53
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-28 23:38:18 +0330

    version 1.1.50

--- src/mime-types.error.ts
+++ src/mime-types.error.ts
@@ -9,7 +9,8 @@ export const  MimeTypeError   =  {
   "SLUG_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'SLUG_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED" :{ section , info : 'EXTENSION_FIELD_MIME_TYPES_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "PREFIX_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'PREFIX_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 }
+  "EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED" :{ section , info : 'EXTENSION_FILED_MIME_TYPES_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "PATTERN_MIME_TYPE_ID_IS_WRONG" :{ section , info : 'PATTERN_MIME_TYPE_ID_IS_WRONG' , msg : '', code : 1008 }
 
 
 
