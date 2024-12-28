6416e8a695197a61c48c993df4ff659bab02780f
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-28 22:03:20 +0330

    version 1.1.48

--- src/file.error.ts
+++ src/file.error.ts
@@ -2,8 +2,8 @@ const section ='File'
 export const  FileError   =  {
   "FILE_SECTION_NOT_FOUND" :{ section , info : 'FILE_SECTION_NOT_FOUND' , msg : '', code : 1000 } ,
   "FILE_STORAGE_NOT_FOUND" :{ section , info : 'FILE_STORAGE_NOT_FOUND' , msg : '', code : 1000 } ,
-  "SECTION_FIELD_FILE_SECTION_IS_REQUIRED" :{ section , info : 'SECTION_FIELD_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "SECTION_FIELD_FILE_SECTION_IS_EMPTY" :{ section , info : 'SECTION_FIELD_FILE_SECTION_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "SECTION_FIELD_SECTION_IS_REQUIRED" :{ section , info : 'SECTION_FIELD_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SECTION_FIELD_SECTION_IS_EMPTY" :{ section , info : 'SECTION_FIELD_FILE_SECTION_IS_EMPTY' , msg : '' , code : 1001 } ,
   "PATTERN_TEMP_BUCKET_ID_IS_FIELD_FILE_SECTION_IS_WRONG" :{ section , info : 'PATTERN_TEMP_BUCKET_ID_IS_FIELD_FILE_SECTION_IS_WRONG' , msg : '', code : 1008 } ,
   "MINIMUM_CHARACHTERS_PREFIX_FIELD_FILE_SECTION_IS_3" :{ section , info : 'MINIMUM_CHARACHTERS_PREFIX_FIELD_FILE_SECTION_IS_3' , msg : '' , code : 1001 } ,
   "MAXIMUM_CHARACHTERS_PREFIX_FIELD_FILE_SECTION_IS_3" :{ section , info : 'MAXIMUM_CHARACHTERS_PREFIX_FIELD_FILE_SECTION_IS_3' , msg : '' , code : 1001 } ,
@@ -16,7 +16,8 @@ export const  FileError   =  {
   "HEIGHT_FIELD_FILE_SECTION_IS_REQUIRED" :{ section , info : 'HEIGHT_FIELD_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "MINIMUM_ITEMS_THUMBNAIL_FIELD_FILE_SECTION_IS_1" :{ section , info : 'MINIMUM_ITEMS_THUMBNAIL_FIELD_FILE_SECTION_IS_1' , msg : '' , code : 1001 } ,
   "MINIMUM_ITEMS_MIME_TYPES_FIELD_FILE_SECTION_IS_1" :{ section , info : 'MINIMUM_ITEMS_MIME_TYPES_FIELD_FILE_SECTION_IS_1' , msg : '' , code : 1001 } ,
-  "STATUS_FIELD_FILE_SECTION_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 }
+  "STATUS_FIELD_FILE_SECTION_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "TYPE_SECTION_FILE_SECTION_IS_REQUIRED" :{ section , info : 'TYPE_SECTION_FILE_SECTION_IS_REQUIRED' , msg : '' , code : 1001 }
 
 
 
