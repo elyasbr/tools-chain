857bb3c47bc774fcb7cb54d854ffc4cc98b34600
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-29 23:59:27 +0330

    version 1.1.61

--- src/app-access.error.ts
+++ src/app-access.error.ts
@@ -7,9 +7,6 @@ export const  AppAccessError   =  {
   "APP_NAME_FIELD_APP_ACCESS_IS_EMPTY" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_EMPTY' , msg : '' , code : 1001 } ,
   "IP_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'IP_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "API_KEY_FIELD_APP_ACCESS_IS_EMPTY" :{ section , info : 'API_KEY_FIELD_APP_ACCESS_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "API_KEY_FIELD_APP_ACCESS_IS_DUPLICATED" :{ section , info : 'API_KEY_FIELD_APP_ACCESS_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "PREFIX_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'PREFIX_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "PREFIX_FIELD_APP_ACCESS_IS_EMPTY" :{ section , info : 'PREFIX_FIELD_APP_ACCESS_IS_EMPTY' , msg : '' , code : 1001 }
-
+  "API_KEY_FIELD_APP_ACCESS_IS_DUPLICATED" :{ section , info : 'API_KEY_FIELD_APP_ACCESS_IS_DUPLICATED' , msg : '' , code : 1009 }
 }
 
