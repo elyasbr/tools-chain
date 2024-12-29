8fbcce3e802c8e4ef1ca5785d414b565e2edd7f0
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-29 19:13:27 +0330

    version 1.1.58

--- src/app-access.error.ts
+++ src/app-access.error.ts
@@ -1,6 +1,6 @@
 const section ='Arch'
 export const  AppAccessError   =  {
-  "APP_NOT_FOUND" :{ section , info : 'APP_NOT_FOUND' , msg : '' , code : 1000 } ,
+  "APP_ACCESS_NOT_FOUND" :{ section , info : 'APP_ACCESS_NOT_FOUND' , msg : '' , code : 1000 } ,
   "APP_NAME_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "APP_NAME_FIELD_APP_ACCESS_IS_DUPLICATED" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "STATUS_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
