4c525073dffcb12a88c6e0ca39eecc9971649acb
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-29 09:41:05 +0330

    version 1.1.53

--- src/app-access.error.ts
+++ src/app-access.error.ts
@@ -3,7 +3,8 @@ export const  AppAccessError   =  {
   "APP_NOT_FOUND" :{ section , info : 'APP_NOT_FOUND' , msg : '' , code : 1000 } ,
   "APP_NAME_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "APP_NAME_FIELD_APP_ACCESS_IS_DUPLICATED" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "STATUS_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 }
+  "STATUS_FIELD_APP_ACCESS_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_APP_ACCESS_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "APP_NAME_FIELD_APP_ACCESS_IS_EMPTY" :{ section , info : 'APP_NAME_FIELD_APP_ACCESS_IS_EMPTY' , msg : '' , code : 1001 }
 
 }
 
