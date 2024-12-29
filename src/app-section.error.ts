42152afc3b1eb97c844d28ef8978639d8ade1477
elyasbr <78977021+elyasbr@users.noreply.github.com>
2024-12-29 11:07:38 +0330

    version 1.1.57

--- src/app-section.error.ts
+++ src/app-section.error.ts
@@ -4,7 +4,8 @@ export const  AppSectionError   =  {
   "SLUG_FIELD_APP_SECTION_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_APP_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "STATUS_FIELD_APP_SECTION_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_APP_SECTION_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "PATTERN_APP_ACCESS_ID_IS_WRONG" :{ section , info : 'PATTERN_APP_ACCESS_ID_IS_WRONG' , msg : '', code : 1006 } ,
-  "SLUG_FIELD_APP_SECTION_IS_EMPTY" :{ section , info : 'SLUG_FIELD_APP_SECTION_IS_EMPTY' , msg : '' , code : 1002 }
+  "SLUG_FIELD_APP_SECTION_IS_EMPTY" :{ section , info : 'SLUG_FIELD_APP_SECTION_IS_EMPTY' , msg : '' , code : 1002 } ,
+  "SLUG_FIELD_APP_SECTION_IS_DUPLICATED" :{ section , info : 'SLUG_FIELD_APP_SECTION_IS_DUPLICATED' , msg : '' , code : 1009 }
 
 
 }
