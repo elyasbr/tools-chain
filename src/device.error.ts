138e3143f94c80327e238a42b14a6e7cb87eddbd
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-01 16:41:53 +0330

    version 1.1.79

--- src/device.error.ts
+++ src/device.error.ts
@@ -1,6 +1,7 @@
 const section ='Device'
 export const  DeviceError   =  {
-  "DEVICE_NOT_FOUND" :{ section , info : 'DEVICE_NOT_FOUND' , msg : '' , code : 1000 }
+  "DEVICE_NOT_FOUND" :{ section , info : 'DEVICE_NOT_FOUND' , msg : '' , code : 1000 } ,
+  "THIS_COMPONENT_NOT_FOUND_ANY_DEVICE" :{ section , info : 'THIS_COMPONENT_NOT_FOUND_ANY_DEVICE' , msg : '' , code : 1000 }
 
 }
 
