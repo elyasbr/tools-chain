625d469a69e3e4909c465f9bf29b2895f5322750
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-03 23:06:15 +0330

    version 1.1.82

--- src/front.error.ts
+++ src/front.error.ts
@@ -8,7 +8,8 @@ export const  FrontError   =  {
   "ROUTE_FIELD_IS_EMPTY" :{ section , info : 'SLUG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
   "ROUTE_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 } ,
   "METHOD_FIELD_IS_EMPTY" :{ section , info : 'METHOD_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "METHOD_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'METHOD_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 }
+  "METHOD_FIELD_FRONT_IS_REQUIRED" :{ section , info : 'METHOD_FIELD_FRONT_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "SLUG_FIELD_FRONT_IS_DUPLICATED" :{ section , info : 'SLUG_FIELD_FRONT_IS_DUPLICATED' , msg : '' , code : 1009 }
 
 
 }
