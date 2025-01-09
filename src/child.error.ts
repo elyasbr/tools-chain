4eb4123add0680a013cfe57140b3809a473fb93b
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-09 16:16:43 +0330

    version 1.1.91

--- src/child.error.ts
+++ src/child.error.ts
@@ -5,7 +5,11 @@ export const  ChildError   =  {
   "TAG_FIELD_COMPONENT_IS_REQUIRED" :{ section , info : 'TAG_FIELD_COMPONENT_IS_REQUIRED' , msg : '' , code : 1002 } ,
   "TAG_FIELD_IS_EMPTY" :{ section , info : 'TAG_FIELD_IS_EMPTY' , msg : '' , code : 1001 } ,
   "PATTERN_FIELD_CHILD_ID_IS_WRONG" :{ section , info : 'PATTERN_FIELD_CHILD_ID_IS_WRONG' , msg : '', code : 1005 } ,
-  "QUALITY_FIELD_COMPONENT_IS_REQUIRED" :{ section , info : 'QUALITY_FIELD_COMPONENT_IS_REQUIRED' , msg : '' , code : 1002 }
+  "QUALITY_FIELD_CHILD_IS_REQUIRED" :{ section , info : 'QUALITY_FIELD_CHILD_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "WIDTH_FIELD_CHILD_IS_REQUIRED" :{ section , info : 'WIDTH_FIELD_CHILD_IS_REQUIRED' , msg : '' , code : 1002 }  ,
+  "HEIGHT_FIELD_CHILD_IS_REQUIRED" :{ section , info : 'HEIGHT_FIELD_CHILD_IS_REQUIRED' , msg : '' , code : 1002 }
+
+
 
 }
 
