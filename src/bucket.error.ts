dfc0d3d853e256904091b1352d5fcdf36bb55291
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-09 16:11:36 +0330

    version 1.1.89

--- src/bucket.error.ts
+++ src/bucket.error.ts
@@ -5,7 +5,8 @@ export const  BucketError   =  {
   "SLUG_BUCKET_IS_DUPLICATED" :{ section , info : 'SLUG_BUCKET_IS_DUPLICATED' , msg : '' , code : 1008 } ,
   "FILE_IN_OBJECT_ALREADY_EXISTS" :{ section , info : 'FILE_IN_OBJECT_ALREADY_EXISTS' , msg : '' , code : 1008 } ,
   "SLUG_FIELD_BUCKET_IS_EMPTY" :{ section , info : 'SLUG_FIELD_BUCKET_IS_EMPTY' , msg : '' , code : 1002 }  ,
-  "STATUS_FIELD_BUCKET_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BUCKET_IS_REQUIRED' , msg : '' , code : 1002 }
+  "STATUS_FIELD_BUCKET_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_BUCKET_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "PATTERN_FIELD_BUCKET_ID_IS_WRONG" :{ section , info : 'PATTERN_FIELD_BUCKET_ID_IS_WRONG' , msg : '', code : 1005 }
 
 
 
