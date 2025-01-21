34f7eddb776c85fbc37274752dd8613cc29e43e4
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-21 11:33:09 +0330

    version 1.2.0

--- src/country.error.ts
+++ src/country.error.ts
@@ -2,8 +2,6 @@ const section ='Country'
 export const  CountryError   =  {
   "COUNTRY_NOT_FOUND" :{ section , info : 'COUNTRY_NOT_FOUND' , msg : '', code : 1000 } ,
   "SLUG_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "ISO2_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO2_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1002 } ,
-  "ISO3_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO3_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1003 } ,
   "STATUS_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1004 } ,
   "COUNTRY_BANK_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_NOT_FOUND' , msg : '', code : 1005 } ,
   "COUNTRY_BANK_CRYPTO_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_NOT_FOUND' , msg : '', code : 1006 } ,
@@ -13,15 +11,19 @@ export const  CountryError   =  {
   "PATTERN_COUNTRY_BANK_ID_IS_WRONG" :{ section , info : 'PATTERN_COUNTRY_BANK_ID_IS_WRONG' , msg : '', code : 1010 } ,
   "PATTERN_COUNTRY_BANK_CRYPTO_ID_IS_WRONG" :{ section , info : 'PATTERN_COUNTRY_BANK_CRYPTO_ID_IS_WRONG' , msg : '', code : 1011 } ,
   "SLUG_COUNTRY_IS_DUPLICATED" :{ section , info : 'SLUG_COUNTRY_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "ISO2_COUNTRY_IS_DUPLICATED" :{ section , info : 'ISO2_COUNTRY_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-  "ISO3_COUNTRY_IS_DUPLICATED" :{ section , info : 'ISO3_COUNTRY_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "COUNTRY_AND_BANK_IS_DUPLICATED" :{ section , info : 'COUNTRY_AND_BANK_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "SLUG_COUNTRY_IS_EMPTY" :{ section , info : 'SLUG_COUNTRY_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "ISO2_COUNTRY_IS_EMPTY" :{ section , info : 'ISO2_COUNTRY_IS_EMPTY' , msg : '' , code : 1001 } ,
-  "ISO3_COUNTRY_IS_EMPTY" :{ section , info : 'ISO3_COUNTRY_IS_EMPTY' , msg : '' , code : 1001 } ,
   "COUNTRY_AND_BANK_CRYPTO_IS_DUPLICATED" :{ section , info : 'COUNTRY_AND_BANK_CRYPTO_IS_DUPLICATED' , msg : '' , code : 1009 } ,
   "COUNTRY_AND_BANK_CRYPTO_AND_IPG_IS_DUPLICATED" :{ section , info : 'COUNTRY_AND_BANK_CRYPTO_AND_IPG_IS_DUPLICATED' , msg : '' , code : 1009 } ,
-
+  "COUNTRY_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'COUNTRY_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1001 } ,
+  "COUNTRY_FIELD_COUNTRY_IS_Empty" :{ section , info : 'COUNTRY_FIELD_COUNTRY_IS_Empty' , msg : '' , code : 1001 } ,
+  "ISO2_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO2_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1002 } ,
+  "ISO2_COUNTRY_IS_EMPTY" :{ section , info : 'ISO2_COUNTRY_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "ISO2_COUNTRY_IS_DUPLICATED" :{ section , info : 'ISO2_COUNTRY_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "ISO3_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'ISO3_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1003 } ,
+  "ISO3_COUNTRY_IS_EMPTY" :{ section , info : 'ISO3_COUNTRY_IS_EMPTY' , msg : '' , code : 1001 } ,
+  "ISO3_COUNTRY_IS_DUPLICATED" :{ section , info : 'ISO3_COUNTRY_IS_DUPLICATED' , msg : '' , code : 1009 } ,
+  "STATUS_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1003 }
 
 }
 
