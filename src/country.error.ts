3da3f2bb1300641baa89e76dd67c14b13d55b85b
elyasbr <78977021+elyasbr@users.noreply.github.com>
2025-01-21 11:36:32 +0330

    version 1.2.0

--- src/country.error.ts
+++ src/country.error.ts
@@ -2,7 +2,6 @@ const section ='Country'
 export const  CountryError   =  {
   "COUNTRY_NOT_FOUND" :{ section , info : 'COUNTRY_NOT_FOUND' , msg : '', code : 1000 } ,
   "SLUG_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'SLUG_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1001 } ,
-  "STATUS_FIELD_COUNTRY_IS_REQUIRED" :{ section , info : 'STATUS_FIELD_COUNTRY_IS_REQUIRED' , msg : '' , code : 1004 } ,
   "COUNTRY_BANK_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_NOT_FOUND' , msg : '', code : 1005 } ,
   "COUNTRY_BANK_CRYPTO_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_NOT_FOUND' , msg : '', code : 1006 } ,
   "COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND" :{ section , info : 'COUNTRY_BANK_CRYPTO_IPG_NOT_FOUND' , msg : '', code : 1007 } ,
